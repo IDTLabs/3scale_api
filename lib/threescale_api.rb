a2fea2f77da339af02e5024b4e6848ef725473f0
Robbie Holmes <robbiethegeek@gmail.com>
2015-03-26 13:04:19 -0400

    Create the Threescale module and the API class, and the logic to intiatiate.

--- lib/threescale_api.rb
+++ lib/threescale_api.rb
@@ -1,5 +1,21 @@
 require "threescale_api/version"
 
-module ThreescaleApi
-  # Your code goes here...
+module Threescale
+  class API
+    attr_accessor :providerKey, :url, :path
+    def initialize(providerKey = nil, debug = false)
+      if ENV['THREESCALE_URL']
+        @url = ENV['THREESCALE_URL']
+      else
+        raise Error, "Please set your 3 Scale URL as an environmental variable THREESCALE_URL"
+      end
+      if not providerKey
+        if ENV['THREESCALE_PROVIDER_KEY']
+          providerKey = ENV['THREESCALE_PROVIDER_KEY']
+        end
+        raise Error, "You must provide a 3 Scale provider key" if not providerKey
+        @providerKey = providerKey
+      end
+    end
+  end
 end
