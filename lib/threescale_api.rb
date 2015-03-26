de22321bd9b9e20b717d135ae14a8fb4fe76d2e4
Robbie Holmes <robbiethegeek@gmail.com>
2015-03-26 16:30:09 -0400

    First API call setup get applications.

--- lib/threescale_api.rb
+++ lib/threescale_api.rb
@@ -1,8 +1,9 @@
 require "threescale_api/version"
+require 'faraday'
 
 module Threescale
   class API
-    attr_accessor :providerKey, :url, :path
+    attr_accessor :providerKey, :url, :path, :conn
     def initialize(providerKey = nil, debug = false)
       if ENV['THREESCALE_URL']
         @url = ENV['THREESCALE_URL']
@@ -16,6 +17,15 @@ module Threescale
         raise Error, "You must provide a 3 Scale provider key" if not providerKey
         @providerKey = providerKey
       end
+      @conn = Faraday.new(url = @url) do | faraday|
+        faraday.request :url_encoded
+        faraday.response :logger
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+    def getAccounts
+      response = @conn.get '/admin/api/features.xml', {:provider_key => @providerKey}
+      p "Here is the response: " + response.body
     end
   end
 end
